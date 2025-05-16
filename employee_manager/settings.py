383379654c8f69f18edc452e7680ce519e712fc7
Kar977 <karolmatuszak7@gmail.com>
2025-05-16 20:35:52 +0200

    [FIX] looking for right path to .env in GH actions

--- employee_manager/settings.py
+++ employee_manager/settings.py
@@ -4,7 +4,8 @@ from pydantic import Field, model_validator
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=Path(__file__).resolve().parent /".env") #.parent / ".env")
+    #model_config = SettingsConfigDict(env_file=Path(__file__).resolve().parent /".env") #.parent / ".env")
+    model_config = SettingsConfigDict(env_file=Path(__file__) / ".env")
     postgres_user: str = Field()
     postgres_password: str = Field()
     postgres_host: str = Field()
